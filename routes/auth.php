04218f35f1beee37a8023784c5450c4e8df177e3
saleang <sopheapvisal7@gmail.com>
2026-01-24 12:38:20 +0700

    Update Drop Down Location

--- routes/auth.php
+++ routes/auth.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\LocationController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 // use App\Http\Controllers\RegisterController;
@@ -17,6 +18,12 @@ Route::middleware('guest')->group(function () {
     // Route::get('/check-username', [RegisteredUserController::class, 'checkUsername']);
     Route::get('/check-email', [RegisteredUserController::class, 'checkEmail']);
     Route::get('/check-phone', [RegisteredUserController::class, 'checkPhone']);
+
+    // Location API endpoints for registration form
+    Route::get('/api/provinces', [LocationController::class, 'getProvinces']);
+    Route::get('/api/districts/{provinceId}', [LocationController::class, 'getDistricts']);
+    Route::get('/api/communes/{districtId}', [LocationController::class, 'getCommunes']);
+    Route::get('/api/villages/{communeId}', [LocationController::class, 'getVillages']);
 
     // Login routes
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
